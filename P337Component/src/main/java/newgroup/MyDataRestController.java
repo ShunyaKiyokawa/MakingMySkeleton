5e2e078c6d0bff339594335736df50067a65db42
ryusui_kuzuo <ryusui_kuzuo@main-pc>
2016-10-30 20:19:20 +0900

    Securityを組み込んだものに加えてP337まで組み込んだ。 URLリストを追加

---
+++ P337Component/src/main/java/newgroup/MyDataRestController.java
@@ -0,0 +1,36 @@
+package newgroup;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import newgroup.entity.MyDataEntity;
+
+@RestController
+public class MyDataRestController {
+
+	@Autowired
+	private MyDataService service;
+
+	@Autowired
+	MySampleBean bean;
+
+	@RequestMapping("/rest")
+	public List<MyDataEntity> restAll() {
+		return service.getAll();
+	}
+
+	@RequestMapping("/rest/{num}")
+	public MyDataEntity restBy(@PathVariable int num) {
+		return service.getId(num);
+	}
+
+	@RequestMapping("/count")
+	public int count() {
+		return bean.count();
+	}
+
+}
